9283ab592cb6b95acfb3a9c1c775be5bb4d1cb4a
15712 <15712@LAPTOP-FRN4H6GJ>
2022-08-01 19:43:49 -0400

    use123

---
+++ src/demo/Demo.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Demo {
+public static void main(String[] args) {
+	
+	System.out.println("jave");
+	
+	
+}
+
+// use123
+public void getName(String a,String b) {
+	
+}
+}
